b10852afc768c832f5b1a3028a201283cc7174e1
Hariganesh Srinivasan <hariganeshs1999@gmail.com>
2024-01-01 01:34:54 -0500

    add licence

--- src/main.zig
+++ src/main.zig
@@ -1,62 +1,36 @@
 const std = @import("std");
 const os = std.os;
 const fs = std.fs;
-
-const Termios = struct {
-    tty: *fs.File,
-    termios: os.termios,
-
-    pub fn init(tty: *fs.File) !Termios {
-        return .{ .tty = tty, .termios = try os.tcgetattr(tty.handle) };
-    }
-
-    fn enableRawMode(self: *Termios) !void {
-        var raw = self.termios;
-
-        raw.iflag &= ~@as(os.system.tcflag_t, os.system.BRKINT | os.system.ICRNL | os.system.INPCK | os.system.ISTRIP | os.system.IXON);
-        raw.oflag &= ~@as(os.system.tcflag_t, os.system.OPOST);
-        raw.cflag &= ~@as(os.system.tcflag_t, os.system.CS8);
-        raw.lflag &= ~@as(os.system.tcflag_t, os.system.ECHO | os.system.ICANON | os.system.IEXTEN | os.system.ISIG);
-        raw.cc[os.system.V.MIN] = 0;
-        raw.cc[os.system.V.TIME] = 1;
-
-        try os.tcsetattr(self.tty.handle, .FLUSH, raw);
-    }
-
-    fn disableRawMode(self: *Termios) !void {
-        try os.tcsetattr(self.tty.handle, .FLUSH, self.termios);
-    }
-};
+const root = @import("root.zig");
 
 pub fn main() !void {
-    var tty = try fs.openFileAbsolute("/dev/tty", .{ .mode = .read_write });
-    defer tty.close();
+    var stdout = std.io.getStdOut();
+    var stdin = std.io.getStdIn();
 
-    var termios = try Termios.init(&tty);
+    defer stdin.close();
+    defer stdout.close();
 
-    // disable raw mode on exit
-    defer termios.disableRawMode() catch @panic("Unable to reset termios.");
+    const stdin_reader = stdin.reader().any();
+    const stdout_writer = stdout.writer();
 
+    var termios = try root.termios(&stdin, &stdout, stdout_writer, stdin_reader);
     // enable raw mode
     try termios.enableRawMode();
+    // disable raw mode on exit
+    defer termios.disableRawMode() catch @panic("Unable to reset termios.");
+    // init screen
+    const screen = try termios.getWindowSize();
 
-    var stdin = std.io.getStdIn();
-    var stdin_reader = stdin.reader().any();
-    var stdin_writer = stdin.writer();
-
-    var buffer: [1]u8 = .{0};
+    var editor = root.kilo(stdout_writer, stdin_reader, screen);
 
     while (true) {
-        buffer[0] = 0;
-        _ = try stdin_reader.read(&buffer);
-        const c = buffer[0];
-
-        if (std.ascii.isControl(c)) {
-            try stdin_writer.print("{d}\r\n", .{c});
-        } else {
-            try stdin_writer.print("{d} ('{c}')\r\n", .{ c, c });
+        switch (try editor.processKeyPress()) {
+            .CtrlQPressed => {
+                try editor.refreshScreen();
+                break;
+            },
+            else => {},
         }
-
-        if (c == 'q') break;
+        try editor.refreshScreen();
     }
 }
--- src/root.zig
+++ src/root.zig
@@ -1,10 +1,146 @@
 const std = @import("std");
-const testing = std.testing;
+const os = std.os;
+const fs = std.fs;
 
-export fn add(a: i32, b: i32) i32 {
-    return a + b;
+const KeyResult = enum {
+    CtrlQPressed,
+    Default,
+};
+
+const Screen = struct { row: u16, col: u16 };
+
+pub fn Termios(comptime WriterType: anytype) type {
+    return struct {
+        stdin: *fs.File,
+        stdout: *fs.File,
+        writer: WriterType,
+        reader: std.io.AnyReader,
+        termios: os.termios,
+
+        pub fn init(stdin: *fs.File, stdout: *fs.File, reader: std.io.AnyReader, writer: WriterType) !@This() {
+            return .{
+                .stdin = stdin,
+                .stdout = stdout,
+                .termios = try os.tcgetattr(stdin.handle),
+                .reader = reader,
+                .writer = writer,
+            };
+        }
+
+        pub fn enableRawMode(self: *@This()) !void {
+            var raw = self.termios;
+
+            raw.iflag &= ~@as(os.system.tcflag_t, os.system.BRKINT | os.system.ICRNL | os.system.INPCK | os.system.ISTRIP | os.system.IXON);
+            raw.oflag &= ~@as(os.system.tcflag_t, os.system.OPOST);
+            raw.cflag |= @as(os.system.tcflag_t, os.system.CS8);
+            raw.lflag &= ~@as(os.system.tcflag_t, os.system.ECHO | os.system.ICANON | os.system.IEXTEN | os.system.ISIG);
+            raw.cc[os.system.V.MIN] = 0;
+            raw.cc[os.system.V.TIME] = 1;
+
+            try os.tcsetattr(self.stdin.handle, .FLUSH, raw);
+        }
+
+        pub fn disableRawMode(self: *@This()) !void {
+            try os.tcsetattr(self.stdin.handle, .FLUSH, self.termios);
+        }
+
+        ///
+        /// yoinked from https://github.com/ziglang/zig/blob/4129996211edd30b25c23454520fd78b2a70394b/lib/std/os.zig#L3289
+        ///
+        pub fn getWindowSize(self: *@This()) !Screen {
+            var wsz: os.linux.winsize = undefined;
+
+            const rc = os.linux.syscall3(
+                .ioctl,
+                @as(usize, @bitCast(@as(isize, self.stdout.handle))),
+                os.linux.T.IOCGWINSZ,
+                @intFromPtr(&wsz),
+            );
+
+            return if (os.linux.getErrno(rc) != .SUCCESS or wsz.ws_col == 0) blk: {
+                break :blk if (try self.writer.write("\x1b[999C\x1b[999B") == 12) try self.getCursorPosition() else error.WinSizeSyscallError;
+            } else .{ .row = wsz.ws_row, .col = wsz.ws_col };
+        }
+
+        fn getCursorPosition(self: *@This()) !Screen {
+            if (try self.writer.write("\x1b[6n") != 4) return error.getCursorError;
+
+            var buf: [32]u8 = undefined;
+            var buffered_stream = std.io.fixedBufferStream(&buf);
+            try self.reader.skipBytes(2, .{});
+            try self.reader.streamUntilDelimiter(buffered_stream.writer(), 'R', null);
+
+            // if (buf[0] != '\x1b' or buf[1] != '[') return error.UnexpectedScreenDimFormat;
+
+            var parts = std.mem.splitScalar(u8, buf[0..try buffered_stream.getPos()], ';');
+
+            return .{
+                .row = try std.fmt.parseInt(u16, parts.next().?, 10),
+                .col = try std.fmt.parseInt(u16, parts.next().?, 10),
+            };
+        }
+    };
 }
 
-test "basic add functionality" {
-    try testing.expect(add(3, 7) == 10);
+pub fn termios(stdin: *fs.File, stdout: *fs.File, writer: anytype, reader: std.io.AnyReader) !Termios(@TypeOf(writer)) {
+    return try Termios(fs.File.Writer).init(stdin, stdout, reader, writer);
+}
+
+pub fn kilo(
+    writer: anytype,
+    reader: std.io.AnyReader,
+    screen_size: Screen,
+) Editor(@TypeOf(writer)) {
+    return .{ .writer = writer, .reader = reader, .screen_size = screen_size };
+}
+
+pub fn Editor(comptime WriterType: anytype) type {
+    return struct {
+        writer: WriterType,
+        reader: std.io.AnyReader,
+        screen_size: Screen,
+        buffer: [1]u8 = .{0},
+
+        pub fn readKey(self: *@This()) !u8 {
+            self.buffer[0] = 0;
+            _ = try self.reader.read(self.buffer[0..1]);
+            const c = self.buffer[0];
+
+            if (c != 0) {
+                if (std.ascii.isControl(c)) {
+                    try self.writer.print("{d}\r\n", .{c});
+                } else {
+                    try self.writer.print("{d} ('{c}')\r\n", .{ c, c });
+                }
+            }
+
+            return c;
+        }
+
+        pub fn processKeyPress(self: *@This()) !KeyResult {
+            switch (try self.readKey()) {
+                std.ascii.control_code.xon => return .CtrlQPressed,
+                else => return .Default,
+            }
+        }
+
+        pub fn refreshScreen(self: *@This()) !void {
+            _ = try self.writer.write("\x1b[2J");
+            _ = try self.writer.write("\x1b[H");
+
+            try self.drawRows();
+
+            _ = try self.writer.write("\x1b[H");
+        }
+
+        pub fn drawRows(self: *@This()) !void {
+            for (0..self.screen_size.row) |y| {
+                _ = try self.writer.write("~");
+
+                if (y < self.screen_size.row - 1) {
+                    _ = try self.writer.write("\r\n");
+                }
+            }
+        }
+    };
 }
